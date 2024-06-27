3dccaf622f5bae8fe24479852e4d5711f1f91488
AFINIKI MHYA <johnafinikimhya12@gmail.com>
2024-06-27 10:49:41 +0100

    scroll animation

--- app.js
+++ app.js
@@ -1,24 +1,41 @@
-const body = document.getElementsByClassName(".body");
-const issues = document.querySelectorAll('.issue');
+const body = document.querySelector(".body");
+const issues = document.querySelectorAll(".issue");
+const images = document.querySelectorAll("img");
 
+window.addEventListener("scroll", checkBoxes);
 
+checkBoxes();
+function checkBoxes() {
+  const triggerBottom = (window.innerHeight / 5) * 4;
 
-document.addEventListener('scroll', () => {
-    let closestIssue = null;
-    let closestDistance = Infinity;
-    
-    issues.forEach(issue => {
-        const rect = issue.getBoundingClientRect();
-        const distance = Math.abs(rect.top);
-        
-        if (distance < closestDistance) {
-            closestDistance = distance;
-            closestIssue = issue;
-        }
-    });
+  images.forEach((img) => {
+    const rect = img.getBoundingClientRect();
+    const boxTop = Math.abs(rect.top);
 
-    if (closestIssue) {
-        const color = closestIssue.getAttribute('data-color');
-        document.body.style.backgroundColor = color;
+    if (boxTop < triggerBottom) {
+      img.classList.add("show");
+    } else {
+      img.classList.remove("show");
     }
+  });
+}
+
+document.addEventListener("scroll", () => {
+  let closestIssue = null;
+  let closestDistance = Infinity;
+
+  issues.forEach((issue) => {
+    const rect = issue.getBoundingClientRect();
+    const distance = Math.abs(rect.top);
+
+    if (distance < closestDistance) {
+      closestDistance = distance;
+      closestIssue = issue;
+    }
+  });
+
+  if (closestIssue) {
+    const color = closestIssue.getAttribute("data-color");
+    document.body.style.backgroundColor = color;
+  }
 });
